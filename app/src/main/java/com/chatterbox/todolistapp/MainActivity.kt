9e8efd0d82568e75aaef627f873299c0c3a1fb94
Michael Duruji <mcduruji@hotmail.com>
2025-11-16 13:36:19 -0500

    creates item card reusable -v1

--- app/src/main/java/com/chatterbox/todolistapp/MainActivity.kt
+++ app/src/main/java/com/chatterbox/todolistapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chatterbox.todolistapp.ui.theme.TodoListAppTheme
 
 /*
@@ -28,6 +38,7 @@ import com.chatterbox.todolistapp.ui.theme.TodoListAppTheme
 *       •there should be a button to delete a item
 *       •there should be a button that ticks an item: if item is done, the button should be ticked
 * */
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +46,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    LazyColumn(
+                        contentPadding = innerPadding,
+                        modifier = Modifier.fillMaxSize(),
+                        content = {
+
+                        }
                     )
                 }
             }
@@ -45,18 +59,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/*
+* todo: make icons for delete and done
+* */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ItemCard(text: String, modifier: Modifier = Modifier) {
+    var done by remember { mutableStateOf(false) }
+    var textValue by remember { mutableStateOf(text) }
+
+    Card(modifier = modifier.fillMaxWidth().padding(8.dp)) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Box(modifier = Modifier.weight(1f)) {
+                Text(text = textValue)
+            }
+            Box(modifier = Modifier.padding(4.dp)) {
+                Text(text = "delete")
+            }
+            Box(modifier = Modifier.padding(4.dp)) {
+                Text(text = "done")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TodoListAppTheme {
-        Greeting("Android")
+        ItemCard(text = "test")
     }
 }
